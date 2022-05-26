2056ddcf8ae879fdb916ca90128628d160326b58
YukiTakayama <yukitkym1011@gmail.com>
2022-05-26 19:11:36 +0900

    Firebase AuthでのGoogle Login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 
+import { useUser, login, logout } from "../lib/auth";
 import { Header } from "../components/Header";
 
 const Home: NextPage = () => {
+  const user = useUser();
+
+  const handleLogin = (): void => {
+    login().catch((error) => console.error(error));
+  };
+
+  const handleLogout = (): void => {
+    logout().catch((error) => console.error(error));
+  };
+
   return (
     <>
       <Header />
@@ -11,6 +22,19 @@ const Home: NextPage = () => {
       <p>
         <Link href="/todos">Todo Listはこちら</Link>
       </p>
+      <div>
+        {user !== null ? (
+          <>
+            <h2>ログインしている</h2>
+            <button onClick={handleLogout}>ログアウト</button>
+          </>
+        ) : (
+          <>
+            <h2>ログインしていない</h2>
+            <button onClick={handleLogin}>ログイン</button>
+          </>
+        )}
+      </div>
     </>
   );
 };
